cd5bdae25a561c20e53b3d46567a44a14d6806e1
Fernando Kenji Kamei <fkenjikamei@gmail.com>
2017-11-28 21:25:13 -0300

    Implementado o teste de consulta

--- poo20172si/src/dao/ClienteDAO.java
+++ poo20172si/src/dao/ClienteDAO.java
@@ -1,6 +1,7 @@
 package dao;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
@@ -19,11 +20,19 @@ public class ClienteDAO implements IClienteDAO {
 		Statement stmt = this.conexao.createStatement();
 		stmt.executeUpdate("INSERT INTO Cliente(nome, cpf, email) VALUES('"+cliente.getNome()+"','"+cliente.getCpf()+"','"+cliente.getEmail()+"')");
 	}
+
+	public void alterarNome(String cpf, String novoEmail) throws SQLException {
+		Statement stmt = this.conexao.createStatement();
+		String sql = "UPDATE Cliente SET email='"+novoEmail+"' WHERE cpf='"+cpf+"'";
+		stmt.executeUpdate(sql);
+	}
+
+	public ResultSet consultarClientePorNome(String nome) throws SQLException {
+		Statement stmt = this.conexao.createStatement();
+		String sql = "SELECT * FROM Cliente WHERE nome ='"+nome+"'";
+		
+		ResultSet rs = stmt.executeQuery(sql);
+		
+		return rs;
+	}
 }
-
-
-
-
-
-
-
